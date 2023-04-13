e0c5c2035745c682b38d4ec54e6a1cf3e3bfb49c
Aleksander Chojnowski <achojnowski00@gmail.com>
2023-04-13 11:58:15 +0200

    added missing useGameStatus file

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -5,7 +5,11 @@ import { TETROMINOS } from "../tetrominos";
 import { StyledCell } from "./styles/StyledCell";
 
 const Cell = ({ type }) => {
-  return <StyledCell type={type} color={TETROMINOS[type].color} />;
+  return (
+    <StyledCell type={type} color={TETROMINOS[type].color}>
+      {console.log("fds")}
+    </StyledCell>
+  );
 };
 
-export default Cell;
+export default React.memo(Cell);
